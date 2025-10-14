d5cc025358ae6fc50206e4fa87c151978da053d6
Kaushik <kaushik@gmail.com>
2025-10-14 20:32:46 +0530

    update condfig

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://crud-backend-15ir.onrender.com /api/:path*",
+        destination: "https://crud-backend-15ir.onrender.com/api/:path*",
       },
     ];
   },
